6ab0e6853d06fd02df2e589d9bbd4731101ded52
Alice-2307 <adityasharma23july@gmail.com>
2023-09-23 01:28:24 +0530

    Optimising the Chat App

--- chatapp-backend/controllers/message.js
+++ chatapp-backend/controllers/message.js
@@ -1,5 +1,7 @@
 const messageModel = require("../models/message");
 const userModel = require("../models/user");
+const Sequelize = require("sequelize");
+
 
 exports.userMessage = async (req, res, next) => {
     try {
@@ -8,7 +10,7 @@ exports.userMessage = async (req, res, next) => {
             message: msg,
             userId: req.user.id
         })
-        res.status(201).json({ Message: 'Send Successfully' });
+        res.status(201).json({ Message: data });
     } catch (err) {
         console.log(err);
         res.status(500).json({ Error: 'An error occurred' });
@@ -17,13 +19,28 @@ exports.userMessage = async (req, res, next) => {
 
 exports.getUserMessage = async (req, res, next) => {
     try {
-        const allMessage = await messageModel.findAll({
-            include: [{ model: userModel, attributes: ['name', 'email'] }]
-        });
+        const lastMessage = +req.query.lastmessageid;
+        let allMessage;
+        if (lastMessage === 0) {
+            allMessage = await messageModel.findAll({
+                include: [{ model: userModel, attributes: ['name', 'email'] }]
+            });
+        }
+        else {
+            allMessage = await messageModel.findAll({
+                where: {
+                    id: {
+                        [Sequelize.Op.gt]: lastMessage
+                    }
+                },
+                include: [{ model: userModel, attributes: ['name', 'email'] }]
+            });
+        }
         const userMessages = allMessage.map((message) => ({
             username: message.user.email,
             sender: message.user.name,
             message: message.message,
+            id: message.id
         }));
         res.status(200).json({ Message: userMessages });
     }
--- chatapp-backend/public/chat.js
+++ chatapp-backend/public/chat.js
@@ -3,6 +3,8 @@ const msg = document.getElementById("message");
 const msgdetail = document.getElementById("message-data");
 const message = document.getElementById("message");
 const loginUser = localStorage.getItem('username');
+const checkLocal = JSON.parse(localStorage.getItem('message')) || [];
+const token = document.cookie.split("=")[1];
 
 chat.addEventListener("submit", async (e) => {
     try {
@@ -10,21 +12,22 @@ chat.addEventListener("submit", async (e) => {
         const msgData = {
             message: msg.value
         }
-        const token = document.cookie.split("=")[1];
-        await axios.post("http://localhost:5000/message", msgData, { headers: { "Authorization": token } });
-        const subElement = document.createElement("p");
-        subElement.textContent = `You: ${msg.value}`;
-        msgdetail.appendChild(subElement);
-        msg.value = "";
+        const result = await axios.post("http://localhost:5000/message", msgData, { headers: { "Authorization": token } });
+        postMessage(result.data.Message);
     } catch (error) {
-        if (error.response !== undefined && (error.response.status === 401 || error.response.status === 500)) {
-            message.textContent = `Error: ${error.response.data.Error}`;
-        }
-        else {
-            message.textContent = `Error: ${error.message}`
-        }
+        showError(error)
     }
 })
+function postMessage(msgVal) {
+
+    msg.value = "";
+    checkLocal.push({ id: msgVal.id, message: msgVal.message })
+    localStorage.setItem('message', JSON.stringify(checkLocal))
+    if (checkLocal.length > 10) {
+        checkLocal.shift();
+    }
+    showLocal();
+}
 
 window.addEventListener("DOMContentLoaded", () => {
     loadMessage();
@@ -32,31 +35,69 @@ window.addEventListener("DOMContentLoaded", () => {
 
 async function loadMessage() {
     try {
-        const token = document.cookie.split("=")[1];
-        const messageData = await axios.get("http://localhost:5000/message", { headers: { "Authorization": token } });
-        msgdetail.textContent = "";
-        for (let i = 0; i < messageData.data.Message.length; i++) {
-            showMessage(messageData.data.Message[i], loginUser);
-        }
-    } catch (error) {
-        if (error.response !== undefined && error.response.status === 500) {
-            message.textContent = `Error: ${error.response.data.Error}`;
+        let lastMessageId;
+        if (checkLocal.length === 0) {
+            lastMessageId = 0;
         }
         else {
-            message.textContent = `Error: ${error.message}`
+            const lastMessage = checkLocal[checkLocal.length - 1]
+            lastMessageId = lastMessage.id
         }
+        const messageData = await axios.get(`http://localhost:5000/message?lastmessageid=${lastMessageId}`, { headers: { "Authorization": token } });
+        storedLocal(messageData.data.Message);
+
+    } catch (error) {
+        showError(error)
+    }
+}
+function storedLocal(val) {
+    for (let i = 0; i < val.length; i++) {
+        checkLocal.push({ id: val[i].id, message: val[i].message, sender: val[i].sender, username: val[i].username })
+        localStorage.setItem('message', JSON.stringify(checkLocal))
+        if (checkLocal.length > 10) {
+            checkLocal.shift();
+        }
+    }
+    showLocal();
+}
+
+function showLocal() {
+
+    msgdetail.textContent = "";
+    for (let i = 0; i < checkLocal.length; i++) {
+        showMessage(checkLocal[i]);
     }
 }
 
-function showMessage(msgVal, checkUser) {
+function showMessage(msgVal) {
+
     const subElement = document.createElement("p");
-    if (msgVal.username === checkUser) {
+    if (msgVal.message === 'joined') {
+        if (msgVal.username === undefined || msgVal.username === loginUser) {
+            subElement.textContent = `You ${msgVal.message}`;
+        }
+        else {
+            subElement.textContent = `${msgVal.sender} ${msgVal.message}`;
+        }
+    }
+    else if (msgVal.username === undefined) {
         subElement.textContent = `You: ${msgVal.message}`;
     }
     else {
         subElement.textContent = `${msgVal.sender}: ${msgVal.message}`;
     }
     msgdetail.appendChild(subElement);
+
 }
+
+function showError(error) {
+    if (error.response !== undefined && (error.response.status === 401 || error.response.status === 500)) {
+        message.textContent = `Error: ${error.response.data.Error}`;
+    }
+    else {
+        message.textContent = `Error: ${error.message}`
+    }
+}
+
 
 setInterval(loadMessage, 1000);
